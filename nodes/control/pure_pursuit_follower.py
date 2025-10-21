d25a9539a2562dbd0927fe7e8e90ce411ed1c62b
Kaisa Talts <kaisatalts@gmail.com>
2025-10-21 15:57:27 +0300

    P3 T5 done

--- nodes/control/pure_pursuit_follower.py
+++ nodes/control/pure_pursuit_follower.py
@@ -6,12 +6,16 @@ from autoware_mini.msg import Path, VehicleCmd
 from geometry_msgs.msg import PoseStamped
 from shapely.geometry import LineString, Point
 from shapely import prepare, distance
+from tf.transformations import euler_from_quaternion
+import numpy as np
 
 class PurePursuitFollower:
     def __init__(self):
 
         # Parameters
         self.path_linestring = None
+        self.lookahead_distance = rospy.get_param("~lookahead_distance")
+        self.wheel_base = rospy.get_param("/vehicle/wheel_base")
 
         # Publishers
         self.vehicle_cmd_pub = rospy.Publisher('/control/vehicle_cmd', VehicleCmd, queue_size=10)
@@ -35,11 +39,21 @@ class PurePursuitFollower:
         vehicle_cmd.header.frame_id = "base_link"
 
         current_pose = Point([msg.pose.position.x, msg.pose.position.y])
+
+        d_ego_from_path_start = 0
         if self.path_linestring is not None:
             d_ego_from_path_start = self.path_linestring.project(current_pose)
             print(d_ego_from_path_start)
+        
+        _, _, heading = euler_from_quaternion([msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z, msg.pose.orientation.w])
+        
+        lookahead_point = self.path_linestring.interpolate(d_ego_from_path_start + self.lookahead_distance)
+        # lookahead point heading calculation
+        lookahead_heading = np.arctan2(lookahead_point.y - current_pose.y, lookahead_point.x - current_pose.x)
+        ld = np.sqrt((lookahead_point.x - current_pose.x)**2 + (lookahead_point.y - current_pose.y)**2)
+        steering_angle = np.arctan(2 * self.wheel_base * np.sin(lookahead_heading - heading) / ld)
 
-        vehicle_cmd.ctrl_cmd.steering_angle = 0.2
+        vehicle_cmd.ctrl_cmd.steering_angle = steering_angle
         vehicle_cmd.ctrl_cmd.linear_velocity = 10.0
         self.vehicle_cmd_pub.publish(vehicle_cmd)
 
