e1f4ed4fc7de8fbd7e6da070ddaba1da70ca5382
Kaisa Talts <kaisatalts@gmail.com>
2025-10-21 14:49:35 +0300

    P3 T1 + T2

---
+++ nodes/control/pure_pursuit_follower.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import rospy
+
+from autoware_mini.msg import Path
+from geometry_msgs.msg import PoseStamped
+
+class PurePursuitFollower:
+    def __init__(self):
+
+        # Parameters
+
+        # Publishers
+
+        # Subscribers
+        rospy.Subscriber('path', Path, self.path_callback, queue_size=1)
+        rospy.Subscriber('/localization/current_pose', PoseStamped, self.current_pose_callback, queue_size=1)
+
+    def path_callback(self, msg):
+        # TODO
+        pass
+
+    def current_pose_callback(self, msg):
+        # TODO
+        print(f"x: {msg.pose.position.x}, y: {msg.pose.position.y}")
+
+    def run(self):
+        rospy.spin()
+
+if __name__ == '__main__':
+    rospy.init_node('pure_pursuit_follower')
+    node = PurePursuitFollower()
+    node.run()
