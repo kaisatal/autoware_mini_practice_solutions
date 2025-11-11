5a14fa5c1eea151f2563523e33248aa9e45bd109
Kaisa Talts <kaisatalts@gmail.com>
2025-11-11 14:58:26 +0200

    minor (added node name to global path reached log message)

--- nodes/planning/global/lanelet2_global_planner.py
+++ nodes/planning/global/lanelet2_global_planner.py
@@ -72,7 +72,7 @@ class GlobalPlanner:
         
         if lanelet2.geometry.distance(self.current_location, self.goal_point) < self.distance_to_goal_limit:
             self.publish_waypoints([]) # Empty list of waypoints
-            rospy.loginfo("Goal reached, path cleared.")
+            rospy.loginfo("%s - Goal reached, path cleared.", rospy.get_name())
             self.goal_point = None
 
     def goal_callback(self, msg):
